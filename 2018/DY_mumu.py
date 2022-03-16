198d010a6e4bebdc65e1804645f59a3ca4cdd7b6
Efe <efe.yazgan@cern.ch>
2022-03-16 17:21:41 +0100

    further updates

--- 2018/DY_mumu.py
+++ 2018/DY_mumu.py
@@ -229,7 +229,7 @@ for f in ['ttZZ.root']:
 
 #2018OK
 TTTo2L_list = ROOT.std.vector('string')()
-for f in ['TTTo2L.root','TTTo2L_1.root']:
+for f in ["TTTo2L.root","TTTo2L_1.root"]:
   TTTo2L_list.push_back(path+f)
 
 #2018OK
