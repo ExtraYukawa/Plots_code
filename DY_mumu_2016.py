c9d1251d1e05eb141e715efbd4cee420f3d1f837
Gouranga Kole <gouranga.tifr@gmail.com>
2022-05-05 12:58:16 +0200

    trigger updated

--- DY_mumu_2016.py
+++ DY_mumu_2016.py
@@ -88,7 +88,11 @@ def for_singlemuon_trigger_mumuchannel(df):
   return single_mu_trigger
 
 def for_singlemuon_trigger_emuchannel(df):
-  single_mu_trigger = df.Filter("!(HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ || HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ || HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ) && HLT_IsoMu27")
+    if opts.subEra == "APV":
+    single_mu_trigger = df.Filter("!(HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL || HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL || HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL) && HLT_IsoMu27")
+  elif opts.subEra == "postAPV":
+    single_mu_trigger = df.Filter("!(HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ || HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ || HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ) && HLT_IsoMu27")
+
   return single_mu_trigger
 
 def for_cross_trigger(df):
