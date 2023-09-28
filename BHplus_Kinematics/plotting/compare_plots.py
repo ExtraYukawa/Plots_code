4def08fbc47c0b6b258f3de8ed30dc716217b6a9
Gouranga Kole <gouranga.tifr@gmail.com>
2023-09-28 10:59:16 +0200

    signal ordering fixed

--- BHplus_Kinematics/plotting/compare_plots.py
+++ BHplus_Kinematics/plotting/compare_plots.py
@@ -49,7 +49,13 @@ histo_xtitle = {
     "DeepB_loose_j3_pt"  : "3^{rd} leading b-jet p_{T}",
     "DeepB_loose_j1_eta" : "Leading b-jet #eta",
     "DeepB_loose_j2_eta" : "2^{nd} leading b-jet #eta",
-    "DeepB_loose_j3_eta" : "3^{rd} leading b-jet #eta"
+    "DeepB_loose_j3_eta" : "3^{rd} leading b-jet #eta",
+    "j1_pt"              : "Leading jet p_{T}",
+    "j2_pt"              : "2^{nd} leading jet p_{T}",
+    "j3_pt"              : "3^{rd} leading jet p_{T}",
+    "j1_eta"             : "Leading jet #eta",
+    "j2_eta"             : "2^{nd} leading jet #eta",
+    "j3_eta"             : "3^{rd} leading jet #eta"
 }
 
 
@@ -63,10 +69,9 @@ def makePlot(folder, hname, xmin, xmax, year="2018", isNorm=True):
     masses = ["200","350","500","800","1000"]
     for mass in masses:
         #print ("adding histogram for mass: %s GeV",%(mass))
-        print ("mass: ", mass)
+        # print ("mass: ", mass)
         h_all[mass] = root_file.Get("%s"%hname+"_"+mass)
     
-
     if wRatio:
         canvas = RatioCanvas(" "," ",41500)
         canvas.ytitle = 'Normalized to Unity'
@@ -77,13 +82,11 @@ def makePlot(folder, hname, xmin, xmax, year="2018", isNorm=True):
         canvas.xtitle = 'Jet Pt'
     canvas.legend.setPosition(0.7, 0.7, 0.9, 0.9)
 
-    # sort the dict (NOT working yet)
-    # h_sorted = {i: h_all[i] for i in sorted(h_all)}
-    # print (h_sorted)
-
-    for key in h_all:
-        h_all[key].Scale(1.0/h_all[key].Integral()) #normalize histogram
+    # Adding to canvas
+    for i, key in enumerate(h_all):
+        key = masses[i]
         print ("key: ", key)
+        h_all[key].Scale(1.0/h_all[key].Integral()) #normalize histogram
         canvas.legend.add(h_all[key], title = key, opt = 'LP', color = colors[key], fstyle = 0, lwidth = 2)
         canvas.addHistogram(h_all[key], drawOpt = 'HIST E')
 
@@ -95,11 +98,17 @@ def makePlot(folder, hname, xmin, xmax, year="2018", isNorm=True):
 
 # Main
 if __name__ == "__main__":
-    
     # plottting
-    makePlot("2017_mujets_20Sep2023T1457","DeepB_loose_j1_pt",30.0,200.0)
-    makePlot("2017_mujets_20Sep2023T1457","DeepB_loose_j2_pt",30.0,200.0)
-    makePlot("2017_mujets_20Sep2023T1457","DeepB_loose_j3_pt",30.0,200.0)
-    makePlot("2017_mujets_20Sep2023T1457","DeepB_loose_j1_eta",-2.5,2.5)
-    makePlot("2017_mujets_20Sep2023T1457","DeepB_loose_j2_eta",-2.5,2.5)
-    makePlot("2017_mujets_20Sep2023T1457","DeepB_loose_j3_eta",-2.5,2.5)
+    # makePlot("2017_sel1_mujets_kinematics_26Sep2023","DeepB_loose_j1_pt",30.0,200.0)
+    # makePlot("2017_sel1_mujets_kinematics_26Sep2023","DeepB_loose_j2_pt",30.0,200.0)
+    # makePlot("2017_sel1_mujets_kinematics_26Sep2023","DeepB_loose_j3_pt",30.0,200.0)
+    # makePlot("2017_sel1_mujets_kinematics_26Sep2023","DeepB_loose_j1_eta",-2.5,2.5)
+    # makePlot("2017_sel1_mujets_kinematics_26Sep2023","DeepB_loose_j2_eta",-2.5,2.5)
+    # makePlot("2017_sel1_mujets_kinematics_26Sep2023","DeepB_loose_j3_eta",-2.5,2.5)
+    makePlot("2017_sel2_mujets_kinematics","j1_pt",30.0,200.0)
+    makePlot("2017_sel2_mujets_kinematics","j1_eta",-2.5,2.5)
+    makePlot("2017_sel2_mujets_kinematics","j2_pt",30.0,200.0)
+    makePlot("2017_sel2_mujets_kinematics","j2_eta",-2.5,2.5)
+    makePlot("2017_sel2_mujets_kinematics","j3_pt",30.0,200.0)
+    makePlot("2017_sel2_mujets_kinematics","j3_eta",-2.5,2.5)
+    
